44e55cad4d71336fe10cefcf4ab0828d8aba69ae
Maikol Reyes <Maikol.Reyes@somosrex.com>
2024-11-01 17:12:42 -0300

    small changes in hero and navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,9 +6,10 @@ export const Navbar = () => {
 
     return (
         <>
-            <nav className="navbar navbar-expand-lg d-flex justify-between">
-                <div className="container-fluid d-flex justify-between">
-                    <a className="navbar-brand" href="#inicio"><img src={logo} alt="Logo-empresa" className='logo w-40 m-10' /></a>
+            <nav className="navbar navbar-expand-lg">
+
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#inicio"><img src={logo} alt="Logo-empresa" className='logo' /></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
--- src/layout/Hero.tsx
+++ src/layout/Hero.tsx
@@ -7,10 +7,10 @@ export const Hero = () => {
             <div className='hero' id='inicio'>
                 <div className='hero__content animate__animated animate__backInLeft'>
                     <h2 className='hero__text'>Impulsamos tu negocio al siguiente nivel🚀</h2>
-                    <p className='font-myCustomFontSecondary desktop:text-lg text-black'>Te ayudamos a incrementar la presencia de tu negocio o empresa en internet con nuestros servicios de Publicidad en Redes, Email Marketing y Desarrollo Web. Contáctanos.</p>
-                    <a id='text' className='hero__button rounded-2xl font-myCustomFontSecondary xl:w-40 xl:h-12 2xl:text-xl btn animate__animated animate__backInLeft' href='https://wa.me/message/X65MZULZJUFYL1' target='_blank'>Contactarse</a>
+                    <p className='hero__description'>Te ayudamos a incrementar la presencia de tu negocio o empresa en internet con nuestros servicios de Publicidad en Redes, Email Marketing y Desarrollo Web.</p>
+                    <a id='text' className='btn hero__button animate__animated animate__backInLeft' href='https://wa.me/message/X65MZULZJUFYL1' target='_blank'>Contactarse</a>
                 </div>
-                <img src={hero} width="100%" height="100%" loading="lazy" alt="Presentacion principal" className='mt-16' />
+                <img src={hero} width="100%" height="100%" loading="lazy" alt="Presentacion principal" className='hero__foto' />
             </div>
         </>
     )
